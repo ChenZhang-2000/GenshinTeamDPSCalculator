8fd79ac779e1bb5431dd520df67b37f697c4b74a
Azir Zhang <chenz28@uci.edu>
2021-09-17 20:29:29 +0800

    changed main ui logics

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from common import ui, exl_operation as ExcelOperation
+import openpyxl.utils.exceptions
 
 
 def main():
-    char_dir = input("请输入角色信息表格文件地址：")  # r".\data\characters.xlsx"  #
-    enemy_dir = input("请输入敌人信息表格文件地址：")  # r".\data\enemy.xlsx"  #
-    team_dir = input("请输入队伍技能循环表格文件地址：")  # r".\data\skills.xlsx"  #
+    while True:
+        char_dir = input("请输入角色信息表格文件地址：")  # r".\data\characters.xlsx"  #
+        try:
+            chars = ExcelOperation.load_chars(char_dir)
+            break
+        except openpyxl.utils.exceptions.InvalidFileException:
+            # raise
+            print("无效的文件地址")
 
-    chars = ExcelOperation.load_chars(char_dir)
-    enemies = ExcelOperation.load_enemies(enemy_dir)
-    teams = ExcelOperation.load_skills({char.name: char for char in chars}, team_dir)
+    while True:
+        enemy_dir = input("请输入敌人信息表格文件地址：")  # r".\data\enemy.xlsx"  #
+        try:
+            enemies = ExcelOperation.load_enemies(enemy_dir)
+            break
+        except openpyxl.utils.exceptions.InvalidFileException:
+            print("无效的文件地址")
+
+    while True:
+        team_dir = input("请输入队伍技能循环表格文件地址：")  # r".\data\skills.xlsx"  #
+        try:
+            teams = ExcelOperation.load_skills({char.name: char for char in chars}, team_dir)
+            break
+        except openpyxl.utils.exceptions.InvalidFileException:
+            print("无效的文件地址")
 
     # print(chars)
     ui.main_loop(chars, enemies, teams)
