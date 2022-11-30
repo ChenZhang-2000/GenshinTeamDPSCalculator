ef90446ccf02a20db4c8a499c90aabfc030cfc50
Chen Zhang <chenz28@uci.edu>
2022-11-29 17:45:07 -0800

    added an artifact model

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from tools.artifact_input import ArtifactModel
---
+++ tools/artifact_input.py
@@ -0,0 +1,33 @@
+
+from torch import nn
+
+from common.model import Model
+from common.controller import read_char_excel, read_enemy_excel, read_skill_excel, team_generation
+
+
+class ArtifactModel(nn.Module):
+    def __init__(self, char_dir, enemy_dir, skill_dir):
+        super(ArtifactModel, self).__init__()
+        self.char_data = read_char_excel(char_dir)
+        self.enemy = read_enemy_excel(enemy_dir)[0]
+
+        self.team = team_generation(self.char_data)
+        skill_df, buff_df, infusion_df = read_skill_excel(self.team, skill_dir)
+        self.model = Model(team=self.team, enemy=self.enemy,
+                           skills=skill_df,
+                           buffs=buff_df,
+                           infusions=infusion_df)
+
+    def change_artifact(self, stats):
+        self.char_data['artifact']['stats'] = stats
+        self.team = team_generation(self.char_data)
+        skill_df, buff_df, infusion_df = read_skill_excel(self.team, self.skill_dir)
+        self.model = Model(team=self.team, enemy=self.enemy,
+                           skills=skill_df,
+                           buffs=buff_df,
+                           infusions=infusion_df)
+
+    def forward(self, affix):
+        self.change_artifact(affix)
+        result = self.model.run()[-1]
+        return result
