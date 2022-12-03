812e80dbd22f0295288201ac7463b7381d831f29
Chen Zhang <chenz28@uci.edu>
2022-12-02 23:47:12 -0800

    debugging ArtifactModel

--- GTDC/tools/artifact_input.py
+++ GTDC/tools/artifact_input.py
@@ -8,7 +8,8 @@ from GTDC.common.controller import read_char_excel, read_enemy_excel, read_skill
 class ArtifactModel(nn.Module):
     def __init__(self, char_dir, enemy_dir, skill_dir, char_idx=0):
         super(ArtifactModel, self).__init__()
-        self.char_data = read_char_excel(char_dir)[char_idx]
+        self.char_idx = char_idx
+        self.char_data = read_char_excel(char_dir)
         enemies = read_enemy_excel(enemy_dir)
         self.enemy = enemies[list(enemies.keys())[0]]
 
@@ -20,7 +21,7 @@ class ArtifactModel(nn.Module):
                            infusions=infusion_df)
 
     def change_artifact(self, stats):
-        self.char_data['artifact']['stats'] = stats
+        self.char_data[self.char_idx]['artifact']['stats'] = stats
         self.team = team_generation(self.char_data)
         skill_df, buff_df, infusion_df = read_skill_excel(self.team, self.skill_dir)
         self.model = Model(team=self.team, enemy=self.enemy,
